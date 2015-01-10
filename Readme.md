86b2230b80671979e70b9f23f96f22dbd6494f6e
nulltask <nulltask@gmail.com>
2015-01-11 06:02:18 +0900

    initial commit

---
+++ Readme.md
@@ -0,0 +1,36 @@
+
+# pseudo-function
+
+Turn the function object to URL object. This library is useful to create web workers with inline function.
+
+## Example
+
+```js
+var pfn = require('pseudo-function');
+
+var fn = pfn(function() {
+  setInterval(function() {
+    self.postMessage(new Date());
+  }, 1000);
+});
+
+var worker = new Worker(fn);
+
+worker.onmessage = function(e) {
+  console.log(e.data);
+};
+
+setTimeout(function() {
+  worker.terminate();
+  fn.revoke();  // clean up pseudo function
+  console.log('terminated');
+}, 5000);
+```
+
+## Installation
+
+    $ npm install pseudo-function
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,60 @@
+
+/**
+ * Module dependencies.
+ */
+
+var regex = require('function-body-regex');
+var Blob = require('blob');
+
+/**
+ * Expose `PseudoFunction`.
+ */
+
+module.exports = PseudoFunction;
+
+/**
+ * @param {Function} fn
+ * @api public
+ */
+
+function PseudoFunction(fn) {
+  if (!(this instanceof PseudoFunction)) return new PseudoFunction(fn);
+  var result = regex.exec(fn.toString());
+  if (null == result) throw new TypeError('invalid function is given.');
+  this.fn = fn;
+  this.body = result[1];
+  this.objectURL = null;
+}
+
+/**
+ * @return {String}
+ * @api public
+ */
+
+PseudoFunction.prototype.toString = function() {
+  if (!this.objectURL) {
+    this.objectURL = this.create(this.body);
+  }
+  return this.objectURL;
+};
+
+/**
+ * @return {String}
+ * @api private
+ */
+
+PseudoFunction.prototype.create = function(body) {
+  return URL.createObjectURL(new Blob([body], { type: 'text/script' }));
+};
+
+/**
+ * @return {PseudoFunction}
+ * @api public
+ */
+
+PseudoFunction.prototype.revoke = function() {
+  if (!this.objectURL) return this;
+  URL.revokeObjectURL(this.objectURL);
+  this.objectURL = null;
+  return this;
+};
